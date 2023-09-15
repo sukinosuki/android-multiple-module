dc1fdfb661baf11eb1bb2a4690387c784f1a7bfd
zhouweichuan <1021210354@qq.com>
2023-09-15 15:27:14 +0800

    navigation navigate animation

--- shared-element/src/main/java/com/example/shared_element/ImageFragment.kt
+++ shared-element/src/main/java/com/example/shared_element/ImageFragment.kt
@@ -6,20 +6,35 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsetsController
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.shared_element.databinding.FragmentImageBinding
 
 class ImageFragment : Fragment(R.layout.fragment_image) {
 
-    var _binding: FragmentImageBinding? = null
-    val binding get() = _binding!!
+    private var _binding: FragmentImageBinding? = null
+
+    private val binding get() = _binding!!
+
+    // 接收方式二传参:
+//    val args: ImageFragmentArgs by navArgs<ImageFragmentArgs>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
+//        WindowInsetsControllerCompat(requireActivity().window, requireActivity().window.decorView).apply {
+//            // hide status bar
+//            hide(WindowInsetsCompat.Type.statusBars())
+//            // Allow showing the status bar with swiping from top to bottom
+////            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+//        }
 
-
+//        requireActivity().window.statusBarColor = resources.getColor(R.color.orange, requireActivity().theme)
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
@@ -34,6 +49,7 @@ class ImageFragment : Fragment(R.layout.fragment_image) {
         _binding = FragmentImageBinding.inflate(layoutInflater, container, false)
 
         // 获取参数
+        // 接收方式一传参: arguments(Bundle)获取
         arguments?.getInt("source_id")?.let { id ->
 //            binding.image.transitionName = id.toString()
         }
@@ -47,6 +63,23 @@ class ImageFragment : Fragment(R.layout.fragment_image) {
         arguments?.getString("title")?.let { title ->
             binding.titleText.text = title
         }
+
+        // tip: 动态修改toolbar navigation icon
+        binding.toolbar.navigationIcon?.setTint(requireActivity().resources.getColor(R.color.white, requireActivity().theme))
+
+        binding.toolbar.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        Log.i("hanami", "onCreateView: arguments: $arguments")
+//        Log.i("hanami", "onCreateView: args: $args")
+//
+//        // 接收方式二传参:
+//        binding.titleText.text = args.title
+//        binding.image.setImageResource(args.sourceId)
+//        binding.image.transitionName = args.transitionName
+
+        Log.i("hanami", "onCreateView: ${binding.image.transitionName}")
         val animation =
             TransitionInflater.from(requireContext()).inflateTransition(android.R.transition.move)
 
@@ -55,11 +88,5 @@ class ImageFragment : Fragment(R.layout.fragment_image) {
 
         return binding.root
 //        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
     }
 }
--- shared-element/src/main/java/com/example/shared_element/MainFragment.kt
+++ shared-element/src/main/java/com/example/shared_element/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.example.shared_element
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,6 +22,13 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         _binding = null
     }
 
+    override fun onPause() {
+        super.onPause()
+        Log.i("hanami", "onPause: main fragment pause")
+        // tip: 修改status bar 颜色
+//        requireActivity().window.statusBarColor = resources.getColor(R.color.blue, requireActivity().theme)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,37 +36,65 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     ): View? {
         _binding = FragmentMainBinding.inflate(inflater)
 
+//        requireActivity().window.statusBarColor = resources.getColor(R.color.blue, requireActivity().theme)
+        // 需要提交设置transitionName, 在点击事件里设置会不生效
+        // 条件1
+        val transitionName = "shiroko"
+        binding.image.transitionName = transitionName
+
         binding.image.setOnClickListener {
-            val extras = FragmentNavigatorExtras(binding.image to "transitionName")
+            // 条件2: binding.image to transitionName的transitionName需要和image.transitionName一致
+            val extras = FragmentNavigatorExtras(binding.image to transitionName)
+            // 传参方式一:
             val bundle = Bundle().apply {
                 putInt("source_id", R.drawable.shiroko)
-                putString("transition_name", "transitionName")
+                putString("transition_name", transitionName)
                 putString("title", "shiroko")
             }
-
+            // 方式一
             findNavController().navigate(
                 R.id.action_mainFragment_to_imageFragment,
                 bundle,
                 null,
-                extras
+                extras // TODO: 如果传了extras参数, 过渡动画不会生效
             )
+
+            // TODO:
+            // https://medium.com/@serbelga/shared-elements-in-android-navigation-architecture-component-bc5e7922ecdf
+            // 传参方式二:
+//            val action = MainFragmentDirections.actionMainFragmentToImageFragment(
+//                sourceId = R.drawable.shiroko,
+//                title = "shiroko", transitionName = transitionName
+//            )
+            // 方式二
+//            findNavController().navigate(action, extras)
         }
 
+        val transitionName2 = "shiroko2"
+        binding.image2.transitionName = transitionName2
         binding.image2.setOnClickListener {
-            val extras = FragmentNavigatorExtras(binding.image2 to "transitionName2")
+            val extras = FragmentNavigatorExtras(binding.image2 to transitionName2)
+
             // 传参
             val bundle = Bundle().apply {
                 putInt("source_id", R.drawable.shiroko2)
-                putString("transition_name", "transitionName2")
+                putString("transition_name", transitionName2)
                 putString("title", "shiroko 2")
             }
-
             findNavController().navigate(
                 R.id.action_mainFragment_to_imageFragment,
                 bundle,
                 null,
-                extras
+                null
             )
+
+//            val action = MainFragmentDirections.actionMainFragmentToImageFragment(
+//                sourceId = R.drawable.shiroko2,
+//                title = "shiroko 2",
+//                transitionName = transitionName2
+//            )
+//
+//            findNavController().navigate(action, extras)
         }
 
         return _binding!!.root
